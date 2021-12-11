d4db08060bfb6c825a07fac11d82467023fc6a9b
LittleRedRidingU <para.bola@qq.com>
2021-12-11 14:43:51 -0500

    add nothing

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
 from gensim import corpora, models, similarities, downloader
+import csv
+
+
 # https://radimrehurek.com/gensim/
 
-print("hello world!")
+# word_vectors = downloader.load("word2vec-google-news-300")
+
+
+def main():
+    yo = ["yo", "yo", "yo"]
+
+    f = open('try.csv', 'w')
+    writer = csv.writer(f)
+    writer.writerow(yo)
+    f.close()
+
+
+if __name__ == "__main__":
+    main()
